a52e43520c9d926bfb1b12a5c4610edd0809e779
Dean Norman <dean@22seven.com>
2020-01-30 14:28:53 +0200

    graphQL and SEO fiddling

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,13 @@
  */
 
 module.exports = {
+   siteMetadata: {
+      title: "22seven",
+      description: "Manage your money",
+      author: "@ilove22seven",
+      data: ["item1", "item2"],
+   },
+   data: ["item1", "item2"],
    plugins: [`gatsby-plugin-styled-components`,
    `gatsby-plugin-react-helmet`],
  }
