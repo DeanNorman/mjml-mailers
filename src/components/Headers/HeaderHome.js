cb4db091a49ae8f715107848fc823444ea2b00ef
Dean Norman <dean@22seven.com>
2020-02-13 18:38:18 +0200

    header

---
+++ src/components/Headers/HeaderHome.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const HeaderHome = () => {
+   return (
+      <div> 
+      <div>
+         <h1>Boss your money.</h1>
+         <p>Manage your money more easily and invest in your goals with the free 22seven app.</p>
+         <button>Sign up. It&apos;s free.</button>
+         <button>Watch the video</button>
+      </div> 
+      
+      <div>
+         <img src="header-phone.jpg" alt="" /> 
+         <img src="google-play.jpg" alt=""/>
+         <img src="appstore.jpg" alt=""/>
+      </div> 
+   </div> 
+   )
+}
+
+export default HeaderHome;
