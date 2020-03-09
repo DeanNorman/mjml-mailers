8d9d5e78f67d6742e400a314774e95054a04c5b7
Dean Norman <dean@22seven.com>
2020-03-09 23:57:55 +0200

    contact us page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,6 +4,7 @@ import Layout from '../components/Layout';
 import HeaderContact from '../components/Headers/HeaderContact'
 import SvensContact from '../components/Contact/SvensContact'
 import ContactDetails from '../components/Contact/ContactDetails'
+import Map from '../components/Contact/Map'
 
 const contact = () => {
    return (
@@ -15,48 +16,7 @@ const contact = () => {
          </Helmet>
          <HeaderContact />
          <ContactDetails />
-
-
-         <div> 
-            <img src="mail-icon.jpg" alt=""/>
-         </div>
-
-         <div>
-            <b><p>By email.</p></b><br/>
-            <p>General enquiries and support</p>
-            <a href="#">"thesvens@22seven.com"</a><br/>
-
-            <p>Security</p>
-            <a href="#">"security@22seven.com"</a><br/>
-
-            <p>Media-related stuff or PR</p>
-            <a href="#">"prteam@22seven.com"</a><br/>
-
-            <p>Jobs</p>
-            <a href="#">"awesomejobs@22seven.com"</a><br/>
-         </div>
-
-         <div> 
-            <img src="phone-icon.jpg" alt=""/>
-         </div>
-    
-         <div>
-            <b><p>By phone.</p></b><br/>
-            <p>The Svens</p>
-            <a href="#">"+27 87 550 4331"</a><br/>
-
-            <p>Between 8am and 8pm every day</p>               
-         </div>
-
-         <div> 
-            <b><h2>HQ. Cape Town</h2></b><br/>
-
-            <p>Come meet us in person</p>
-            <p> Find us at 60 Hout Street in Cape Town.</p><br/>
-
-            <a href="#">"Get directions"</a><br/>
-         </div>
-
+         <Map />
          <SvensContact />
       </Layout>
    )
