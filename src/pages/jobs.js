c7221fa10b04cee056149cd4afc33c90996185f4
Dean Norman <dean@22seven.com>
2020-03-30 12:16:42 +0200

    more import fix

--- src/pages/jobs.js
+++ src/pages/jobs.js
@@ -5,7 +5,6 @@ import { Helmet } from 'react-helmet';
 import HeaderJobs from '../components/Headers/HeaderJobs'
 import OurCulture from '../components/Jobs/OurCulture'
 import GeneralContent from '../components/Jobs/GeneralContent'
-import ApplyJobs from '../components/Jobs/ApplyJobs'
 import NoJobs from '../components/Jobs/NoJobs'
 
 const jobs = () => {
@@ -30,7 +29,6 @@ const jobs = () => {
             text="Our HQ is in one of the oldest theatres in South Africa, right in the middle of the Cape Town City Bowl. Imagine if you smashed a temple of amazingness into a really comfortable lounge with great art on the walls. That still wouldn’t be as awesome as our offices. Needless to say, anyone who wants to work with us should be in Cape Town, or plan to be."
             officeImage
          />
-         {/* <ApplyJobs /> */}
       </Layout>
    )
 }
